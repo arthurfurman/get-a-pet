405324c37be3e2469e8cead2e24afef8b9b09d60
Arthur Furman <foarthur@gmail.com>
2021-10-17 14:26:53 +0300

    Create a default admin user if does exists

--- server/db.js
+++ server/db.js
@@ -1,12 +1,26 @@
 // mongo db
 const mongoose = require("mongoose");
+const User = require("./models/User");
 
-const DB_NAME = 'get-a-pet'
+const DB_NAME = "get-a-pet";
 const MONGO_URI = `mongodb://127.0.0.1:27017/${DB_NAME}`;
 
-module.exports = () => mongoose
-    .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(console.log(`MongoDB connected ${MONGO_URI}`))
-    .catch(err => {
-      console.error("Shutting down due to fatal db error during connect!", err)
-    });
+module.exports = () =>
+	mongoose
+		.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+		.then(console.log(`MongoDB connected ${MONGO_URI}`))
+		.then(User.exists({ isAdmin: true }).then((result) => {
+			if (!result) {
+				new User({
+					email: "admin@admin.com",
+					firstName: "admin",
+					lastName: "admin",
+					address: "admin",
+					password: "admin",
+					isAdmin: true,
+				}).save();
+			}
+		}))
+		.catch((err) => {
+			console.error("Shutting down due to fatal db error during connect!", err);
+		});
