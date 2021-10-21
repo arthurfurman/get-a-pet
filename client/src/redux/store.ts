3a10715b726536d25571292c0460f3e731f68ddc
Arthur Furman <foarthur@gmail.com>
2021-10-21 17:30:47 +0300

    Use redux devtools to debug redux state.

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -8,6 +8,6 @@ const rootReducer = combineReducers({
 	pets: petsReducer
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
